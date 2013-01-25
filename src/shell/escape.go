ae30d7612bcc0ae10dd56ae7c4d9019aaa16ff69
Jonas Pfenniger <jonas@pfenniger.name>
2013-01-25 19:19:48 +0000

    Shell escape fixes

--- src/shell/escape.go
+++ src/shell/escape.go
@@ -5,23 +5,23 @@ import (
 	"strings"
 )
 
+var ESCAPE_REG = regexp.MustCompile("([^A-Za-z0-9_\\-.,:/@\n])")
+
 // This function and comments have been copied over from Ruby's
 // stdlib shellwords.rb library.
 func Escape(str string) string {
 	if str == "" {
-		return "''"
+		return ""
 	}
 
 	// Treat multibyte characters as is.  It is caller's responsibility
 	// to encode the string in the right encoding for the shell
 	// environment.
-	r := regexp.MustCompile("([^A-Za-z0-9_\\-.,:/@\n])")
-	replace := func(match string) string { return "\\\\\\" + match }
-	str = r.ReplaceAllStringFunc(str, replace)
+	replace := func(match string) string { return "\\" + match }
+	str = ESCAPE_REG.ReplaceAllStringFunc(str, replace)
 
 	// A LF cannot be escaped with a backslash because a backslash + LF
 	// combo is regarded as line continuation and simply ignored.
 	str = strings.Replace(str, "\n", "'\n'", -1)
 	return str
 }
-
