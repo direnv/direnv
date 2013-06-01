693901cfb7d8be0f189135ab752468410d564184
Jonas Pfenniger <jonas@pfenniger.name>
2013-06-01 13:29:28 +0100

    Change the `direnv edit` logic a bit.
    By default we don't create a new .envrc is no path is provided. This allows
    the command to be less ambiguous.

--- cmd_edit.go
+++ cmd_edit.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 // `direnv edit [PATH_TO_RC]`
 var CmdEdit = &Cmd{
-	Name:   "edit",
-	Desc:   `Opens PATH_TO_RC or the current .envrc or a new .envrc into an $EDITOR and allow the file to be loaded afterwards.`,
+	Name: "edit",
+	Desc: `Opens PATH_TO_RC or the current .envrc into an $EDITOR and allow
+  the file to be loaded afterwards.`,
 	Args:   []string{"[PATH_TO_RC]"},
 	NoWait: true,
 	Fn: func(env Env, args []string) (err error) {
@@ -23,13 +25,16 @@ var CmdEdit = &Cmd{
 
 		if len(args) > 1 {
 			rcPath = args[1]
+			fi, _ := os.Stat(rcPath)
+			if fi != nil && fi.IsDir() {
+				rcPath = filepath.Join(rcPath, ".envrc")
+			}
 		} else {
 			foundRC = config.FindRC()
-			if foundRC != nil {
-				rcPath = foundRC.path
-			} else {
-				rcPath = ".envrc"
+			if foundRC == nil {
+				return fmt.Errorf(".envrc not found. Use `direnv edit .` to create a new envrc in the current directory.")
 			}
+			rcPath = foundRC.path
 		}
 
 		editor := env["EDITOR"]
