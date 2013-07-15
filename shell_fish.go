cb3fc3cb5060b87587eeda58c38b73b46f9b7700
Alan Brenner <alan@abcompcons.com>
2013-07-15 15:39:24 -0400

    initial support for fish

---
+++ shell_fish.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+)
+
+type fish int
+
+var FISH fish
+
+func (f fish) Hook() string {
+	return ""
+}
+
+func (f fish) Escape(str string) string {
+	if str == "" {
+		return "''"
+	}
+	in := []byte(str)
+	out := ""
+	i := 0
+	l := len(in)
+
+	hex := func(char byte) {
+		out += fmt.Sprintf("\\x%02x", char)
+	}
+
+	backslash := func(char byte) {
+		out += string([]byte{BACKSLASH, char})
+	}
+
+	escaped := func(str string) {
+		out += str
+	}
+
+	quoted := func(char byte) {
+		out += string([]byte{char})
+	}
+
+	literal := func(char byte) {
+		out += string([]byte{char})
+	}
+
+	for i < l {
+		char := in[i]
+		switch {
+		case char == TAB:
+			escaped(`\t`)
+		case char == LF:
+			escaped(`\n`)
+		case char == CR:
+			escaped(`\r`)
+		case char <= US:
+			hex(char)
+		case char <= AMPERSTAND:
+			quoted(char)
+		case char == SINGLE_QUOTE:
+			backslash(char)
+		case char <= PLUS:
+			quoted(char)
+		case char <= NINE:
+			literal(char)
+		case char <= QUESTION:
+			quoted(char)
+		case char <= LOWERCASE_Z:
+			literal(char)
+		case char == OPEN_BRACKET:
+			quoted(char)
+		case char == BACKSLASH:
+			backslash(char)
+		case char <= CLOSE_BRACKET:
+			quoted(char)
+		case char == UNDERSCORE:
+			literal(char)
+		case char <= BACKTICK:
+			quoted(char)
+		case char <= LOWERCASE_Z:
+			literal(char)
+		case char <= TILDA:
+			quoted(char)
+		case char == DEL:
+			hex(char)
+		default:
+			hex(char)
+		}
+		i += 1
+	}
+
+	return out
+}
+
+func (f fish) Export(key, value string) string {
+	return "set -x -g " + f.Escape(key) + " " + f.Escape(value) + ";"
+}
+
+func (f fish) Unset(key string) string {
+	return "set -e -g " + f.Escape(key) + ";"
+}
