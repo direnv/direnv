eb3b6100c8f06e42fe9c8183cc4600bd90d60026
zimbatm <zimbatm@zimbatm.com>
2014-02-13 19:54:38 +0000

    If two paths don't have a common ancestors, don't make them relative.
    Fixes #101

--- rc_test.go
+++ rc_test.go
@@ -14,3 +14,16 @@ func TestSomething(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestRootDir(t *testing.T) {
+	var r string
+	r = rootDir("/foo")
+	if r != "/foo" {
+		t.Error(r)
+	}
+
+	r = rootDir("/foo/bar")
+	if r != "/foo" {
+		t.Error(r)
+	}
+}
