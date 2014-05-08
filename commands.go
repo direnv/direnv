3c2f866232e255e2276552c6dc396563d8d0fd53
zimbatm <zimbatm@zimbatm.com>
2014-05-08 09:23:03 +0100

    Bump command timeout to 5 seconds

--- commands.go
+++ commands.go
@@ -78,7 +78,7 @@ func CommandsDispatch(env Env, args []string) error {
 			select {
 			case <-done:
 				return
-			case <-time.After(2 * time.Second):
+			case <-time.After(5 * time.Second):
 				log("(%v) is taking a while to execute. Use CTRL-C to give up.", args)
 			}
 		}()
