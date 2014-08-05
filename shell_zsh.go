3ce73c2fa64b1d876659eee8a050b1578bf47a88
mashiro <mail@mashiro.org>
2014-08-06 02:17:50 +0900

    Add -g option.

--- shell_zsh.go
+++ shell_zsh.go
@@ -9,7 +9,7 @@ const ZSH_HOOK = `
 _direnv_hook() {
   eval "$(direnv export zsh)";
 }
-typeset -a precmd_functions
+typeset -ag precmd_functions
 if [[ -z $precmd_functions[(r)_direnv_hook] ]]; then
   precmd_functions+=_direnv_hook;
 fi
