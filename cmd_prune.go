fce1766319e66b21e45e4ede820f8972c620e5d2
Alexander V. Nikolaev <avn@daemon.hole.ru>
2015-01-19 22:01:19 +0200

    Add new subcommand "prune"
    This subcommand remove "allowing" from .envrc's which no more exists

---
+++ cmd_prune.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+)
+
+var CmdPrune = &Cmd{
+	Name: "prune",
+	Desc: "prune allowed .envrc's which no more exists",
+	Fn: func(env Env, args []string) (err error) {
+		var config *Config
+		var dir *os.File
+		var fi os.FileInfo
+		var dir_list []string
+		var envrc []byte
+
+		if config, err = LoadConfig(env); err != nil {
+			return err
+		}
+
+		allowed := config.AllowDir()
+		if dir, err = os.Open(allowed); err != nil {
+			return err
+		}
+		defer dir.Close()
+
+		if dir_list, err = dir.Readdirnames(0); err != nil {
+			return err
+		}
+
+		for _, hash := range dir_list {
+			filename := path.Join(allowed, hash)
+			if fi, err = os.Stat(filename); err != nil {
+				return err
+			}
+
+			if !fi.IsDir() {
+				if envrc, err = ioutil.ReadFile(filename); err != nil {
+					return err
+				}
+				envrc_str := strings.TrimSpace(string(envrc))
+
+				// skip old files, w/o path inside
+				if envrc_str == "" {
+					continue
+				}
+				if !fileExists(envrc_str) {
+					_ = os.Remove(filename)
+				}
+
+			}
+
+		}
+		return nil
+	},
+}
