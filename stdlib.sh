399b16fd30e5f0ef02c8b8979fff61dd503ae6c1
Ches Martin <ches@whiskeyandgrits.net>
2015-11-12 17:10:56 +0700

    Add layout for projects using the gb build tool for Go

--- stdlib.sh
+++ stdlib.sh
@@ -289,6 +289,20 @@ layout_go() {
   PATH_add bin
 }
 
+# Usage: layout gb
+#
+# Sets up environment for a Go project using the alternative gb build tool. In
+# addition to project executables on PATH, this includes an exclusive, project-
+# local GOPATH which enables many tools like gocode and oracle to "just work".
+#
+# http://getgb.io/
+#
+layout_gb() {
+  export GOPATH="$PWD/vendor:$PWD"
+  PATH_add "$PWD/vendor/bin"
+  PATH_add bin
+}
+
 # Usage: layout node
 #
 # Adds "$PWD/node_modules/.bin" to the PATH environment variable.
