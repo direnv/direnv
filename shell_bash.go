5ac854adc88335ec4e5d978bd5cd434b14b08606
Mike Fiedler <miketheman@gmail.com>
2015-12-25 09:38:33 -0500

    Preserve previous BASH status code
    Credit to @sykesm for reporting in detail.
    
    Fixes #155

--- shell_bash.go
+++ shell_bash.go
@@ -6,7 +6,9 @@ var BASH bash
 
 const BASH_HOOK = `
 _direnv_hook() {
+  local previous_exit_status=$?;
   eval "$(direnv export bash)";
+  return $previous_exit_status;
 };
 if ! [[ "$PROMPT_COMMAND" =~ _direnv_hook ]]; then
   PROMPT_COMMAND="_direnv_hook;$PROMPT_COMMAND";
