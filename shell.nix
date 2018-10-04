e78e393f2340d89f31d2be7b3b95169aa5f528fb
zimbatm <zimbatm@zimbatm.com>
2018-10-04 16:20:01 +0200

    testing: elvish 0.12 is released now (#402)

--- shell.nix
+++ shell.nix
@@ -1,19 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
-let
-  elvish012 = elvish.overrideDerivation (self: {
-    name = "elvis-0.12-rc2";
-
-    src = fetchFromGitHub {
-      owner = "elves";
-      repo = "elvish";
-      rev = "0.12-rc2";
-      sha256 = "1nxijphdbqbv032s9mw7miypy92xdyzkql1bcqh0s1d0ghi66hvm";
-    };
-
-    excludedPackages = [ "github.com/elves/elvish/website" ];
-  });
-in
 mkShell {
   buildInputs = [
     gnumake
@@ -26,7 +12,7 @@ mkShell {
 
     # Shells
     bashInteractive
-    elvish012
+    elvish
     fish
     tcsh
     zsh
