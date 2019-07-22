0bc462cc9f700c4ad0c31a4922eb812f228a4a80
laggardkernel <laggardkernel@users.noreply.github.com>
2019-07-22 17:53:54 +0800

    shell: improve zsh hook (#514)
    Prepend but not append `_direnv_hook` to make sure environment
    variables are changed before any other hooks, in case the vars
    are used by other hooks.
    
    Besides, add `_direnv_hook` into `chpwd` hook, which is triggered
    once working dir is changed by ZLE widget but not command `cd` in ZSH.

--- shell_zsh.go
+++ shell_zsh.go
@@ -11,7 +11,11 @@ _direnv_hook() {
 }
 typeset -ag precmd_functions;
 if [[ -z ${precmd_functions[(r)_direnv_hook]} ]]; then
-  precmd_functions+=_direnv_hook;
+  precmd_functions=( _direnv_hook ${precmd_functions[@]} )
+fi
+typeset -ag chpwd_functions;
+if [[ -z ${chpwd_functions[(r)_direnv_hook]} ]]; then
+  chpwd_functions=( _direnv_hook ${chpwd_functions[@]} )
 fi
 `
 
