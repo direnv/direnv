ca643095883e62f859715022155b2f08ea7e257d
Hideaki Kawai <kayhide@gmail.com>
2021-02-04 14:46:23 +0900

    Create use_env_dir

--- stdlib.sh
+++ stdlib.sh
@@ -1167,6 +1167,26 @@ use_vim() {
   path_add DIRENV_EXTRA_VIMRC "$extra_vimrc"
 }
 
+# Usage: use_env_dir
+#
+# Load environment variables from `$(direnv_layout_dir)/envs" directory.
+# Under this directory, every file is read and set to an environment
+# variable whose name is the filename and value is the file content.
+#
+# Also watch files so to automatially reload on every file update.
+use_env_dir() {
+  local env_dir
+  env_dir="$(direnv_layout_dir)/envs"
+  if [[ -d $env_dir ]]; then
+    for f in "$env_dir"/*; do
+      if [[ -f $f ]]; then
+        watch_file "$f"
+        export "$(basename "$f")=$(cat "$f")"
+      fi
+    done
+  fi
+}
+
 # Usage: direnv_version <version_at_least>
 #
 # Checks that the direnv version is at least old as <version_at_least>.
