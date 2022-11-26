9a9d83c5104e2064c483c6599473d75030cf9d83
david-vicente <57544666+david-vicente@users.noreply.github.com>
2022-11-26 00:33:43 +0000

    Add layout for PDM (python tool)

--- stdlib.sh
+++ stdlib.sh
@@ -1021,6 +1021,31 @@ layout_pyenv() {
   [[ -n "$PYENV_VERSION" ]] && export PYENV_VERSION
 }
 
+# Usage: layout pdm
+#
+# Similar to layout_python, but uses PDM to build a
+# virtualenv from the pyproject.toml located in the same directory.
+#
+layout_pdm() {
+    PYPROJECT_TOML="${PYPROJECT_TOML:-pyproject.toml}"
+    if [ ! -f "$PYPROJECT_TOML" ]; then
+        log_status "No pyproject.toml found. Executing \`pmd init\` to create a \`$PYPROJECT_TOML\` first."
+        pdm init --non-interactive --python "$(python3 --version 2>/dev/null | cut -d' ' -f2 | cut -d. -f1-2)"
+    fi
+
+    VIRTUAL_ENV=$(pdm venv list | grep "^\*"  | awk -F" " '{print $3}')
+
+    if [ -z "$VIRTUAL_ENV" ] || [ ! -d "$VIRTUAL_ENV" ]; then
+        log_status "No virtual environment exists. Executing \`pdm info\` to create one."
+	      pdm info
+        VIRTUAL_ENV=$(pdm venv list | grep "^\*"  | awk -F" " '{print $3}')
+    fi
+
+    PATH_add "$VIRTUAL_ENV/bin"
+    export PDM_ACTIVE=1
+    export VIRTUAL_ENV
+}
+
 # Usage: layout ruby
 #
 # Sets the GEM_HOME environment variable to "$(direnv_layout_dir)/ruby/RUBY_VERSION".
