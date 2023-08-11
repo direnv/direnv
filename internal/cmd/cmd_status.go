100fa25f8399b1a3c098c802176ec4122944a73c
shivaraj-bh <sbh69840@gmail.com>
2023-08-11 20:13:08 +0530

    add --json flag to direnv status

--- internal/cmd/cmd_status.go
+++ internal/cmd/cmd_status.go
@@ -3,40 +3,78 @@ package cmd
 import (
 	"fmt"
 	"path/filepath"
+	"encoding/json"
 )
 
 // CmdStatus is `direnv status`
 var CmdStatus = &Cmd{
 	Name: "status",
 	Desc: "prints some debug status information",
+	Args: []string{"[--json]"},
 	Action: actionWithConfig(func(env Env, args []string, config *Config) error {
-		fmt.Println("direnv exec path", config.SelfPath)
-		fmt.Println("DIRENV_CONFIG", config.ConfDir)
-
-		fmt.Println("bash_path", config.BashPath)
-		fmt.Println("disable_stdin", config.DisableStdin)
-		fmt.Println("warn_timeout", config.WarnTimeout)
-		fmt.Println("whitelist.prefix", config.WhitelistPrefix)
-		fmt.Println("whitelist.exact", config.WhitelistExact)
-
-		loadedRC := config.LoadedRC()
-		foundRC, err := config.FindRC()
-		if err != nil {
-			return err
-		}
-
-		if loadedRC != nil {
-			formatRC("Loaded", loadedRC)
+		if (len(args) > 1 && args[1] == "--json") {
+			loadedRC := config.LoadedRC()
+			foundRC, err := config.FindRC()
+			if err != nil {
+				return err
+			}
+			jsonOutput := map[string]interface{}{
+				"config": map[string]string{
+					"SelfPath": config.SelfPath,
+					"ConfigDir": config.ConfDir,
+				},
+				"state": map[string]interface{}{},
+			}
+			if loadedRC != nil {
+				jsonOutput["state"].(map[string]interface{})["loadedRC"] = map[string]interface{}{
+					"path": loadedRC.path,
+					"allowed": loadedRC.Allowed(),
+				}
+			} else {
+				jsonOutput["state"].(map[string]interface{})["loadedRC"] = nil
+			}
+			if foundRC != nil {
+				jsonOutput["state"].(map[string]interface{})["foundRC"] = map[string]interface{}{
+					"path": foundRC.path,
+					"allowed": foundRC.Allowed(),
+				}
+			} else {
+				jsonOutput["state"].(map[string]interface{})["foundRC"] = nil
+			}
+			jsonBytes, err := json.MarshalIndent(jsonOutput, "", "  ")
+			if err != nil {
+				fmt.Println(err)
+				return nil
+			}
+			fmt.Println(string(jsonBytes))
 		} else {
-			fmt.Println("No .envrc or .env loaded")
-		}
+			fmt.Println("direnv exec path", config.SelfPath)
+			fmt.Println("DIRENV_CONFIG", config.ConfDir)
 
-		if foundRC != nil {
-			formatRC("Found", foundRC)
-		} else {
-			fmt.Println("No .envrc or .env found")
-		}
+			fmt.Println("bash_path", config.BashPath)
+			fmt.Println("disable_stdin", config.DisableStdin)
+			fmt.Println("warn_timeout", config.WarnTimeout)
+			fmt.Println("whitelist.prefix", config.WhitelistPrefix)
+			fmt.Println("whitelist.exact", config.WhitelistExact)
 
+			loadedRC := config.LoadedRC()
+			foundRC, err := config.FindRC()
+			if err != nil {
+				return err
+			}
+
+			if loadedRC != nil {
+				formatRC("Loaded", loadedRC)
+			} else {
+				fmt.Println("No .envrc or .env loaded")
+			}
+
+			if foundRC != nil {
+				formatRC("Found", foundRC)
+			} else {
+				fmt.Println("No .envrc or .env found")
+			}
+		}
 		return nil
 	}),
 }
