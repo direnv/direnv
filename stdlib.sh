2dbfa65fc82baf2bba3c5d5362df73679fa6ccee
Chris Rose <offline@offby1.net>
2024-11-15 22:26:41 -0800

    use --frozen in uv, not --locked
    --frozen doesn't fail if the user is switching between local and stable
    locked dependencies, whereas --locked will bail if so

--- stdlib.sh
+++ stdlib.sh
@@ -1081,10 +1081,10 @@ layout_uv() {
   # Watch the uv configuration file for changes
   watch_file .python-version pyproject.toml uv.lock
 
-  # Use --locked so that direnv entry does not change the lock contents. This
+  # Use --frozen so that direnv entry does not change the lock contents. This
   # can fail if the lock file is badly out of sync, but it'll report that to the
   # user and the resolution is obvious.
-  uv sync --locked || true
+  uv sync --frozen || true
 
   # activate the virtualenv after syncing; this puts the newly-installed
   # binaries on PATH.
