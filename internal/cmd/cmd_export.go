direnv/direnv

commit cf9222e21053889fcc8a1e6c1ca7257c49d86b04

refactor: better error handling for Dump and Error
Forward errors to the users instead of panicking.

internal/cmd/cmd_export.go
@@ -104,7 +104,10 @@ func exportCommand(currentEnv Env, args []string, config *Config) (err error) {
 		logStatus(config, "export %s", out)
 	}
 
-	diffString := currentEnv.Diff(newEnv).ToShell(shell)
+	diffString, err := currentEnv.Diff(newEnv).ToShell(shell)
+	if err != nil {
+		return fmt.Errorf("ToShell() failed: %w", err)
+	}
 	logDebug("env diff %s", diffString)
 	fmt.Print(diffString)
 

internal/cmd/cmd_apply_dump.go
@@ -36,7 +36,10 @@ func cmdApplyDumpAction(env Env, args []string) (err error) {
 
 	diff := env.Diff(dumpedEnv)
 
-	exports := diff.ToShell(Bash)
+	exports, err := diff.ToShell(Bash)
+	if err != nil {
+		return err
+	}
 
 	_, err = fmt.Println(exports)
 	if err != nil {

internal/cmd/cmd_dotenv.go
@@ -58,7 +58,10 @@ func cmdDotEnvAction(_ Env, args []string) (err error) {
 		return err
 	}
 
-	str := newenv.ToShell(shell)
+	str, err := newenv.ToShell(shell)
+	if err != nil {
+		return err
+	}
 	fmt.Println(str)
 
 	return

internal/cmd/cmd_dump.go
@@ -46,7 +46,11 @@ func cmdDumpAction(env Env, args []string) (err error) {
 		return fmt.Errorf("unknown target shell '%s'", target)
 	}
 
-	_, err = fmt.Fprintln(w, shell.Dump(env))
+	dumpStr, err := shell.Dump(env)
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintln(w, dumpStr)
 
 	return
 }

internal/cmd/cmd_watch.go
@@ -51,7 +51,11 @@ func cmdWatchAction(env Env, args []string) (err error) {
 	e := make(ShellExport)
 	e.Add(DIRENV_WATCHES, watches.Marshal())
 
-	os.Stdout.WriteString(shell.Export(e))
+	exportStr, err := shell.Export(e)
+	if err != nil {
+		return err
+	}
+	os.Stdout.WriteString(exportStr)
 
 	return
 }

internal/cmd/cmd_watch_dir.go
@@ -55,7 +55,11 @@ func watchDirCommand(env Env, args []string) (err error) {
 	e := make(ShellExport)
 	e.Add(DIRENV_WATCHES, watches.Marshal())
 
-	os.Stdout.WriteString(shell.Export(e))
+	exportStr, err := shell.Export(e)
+	if err != nil {
+		return err
+	}
+	os.Stdout.WriteString(exportStr)
 
 	return
 }

internal/cmd/cmd_watch_list.go
@@ -76,7 +76,11 @@ func watchListCommand(env Env, args []string) (err error) {
 	e := make(ShellExport)
 	e.Add(DIRENV_WATCHES, watches.Marshal())
 
-	os.Stdout.WriteString(shell.Export(e))
+	exportStr, err := shell.Export(e)
+	if err != nil {
+		return err
+	}
+	os.Stdout.WriteString(exportStr)
 
 	return
 }

internal/cmd/env.go
@@ -83,7 +83,7 @@ func (env Env) ToGoEnv() []string {
 
 // ToShell outputs the environment into an evaluatable string that is
 // understood by the target shell
-func (env Env) ToShell(shell Shell) string {
+func (env Env) ToShell(shell Shell) (string, error) {
 	e := make(ShellExport)
 
 	for key, value := range env {

commit 35031f3a123b9324fc450faeba2a73eed44a6e56

fix regression in displaying export errors
Fixes a regression introduced by:

  cf9222e refactor: better error handling for Dump and Error

At this point in the control flow, `err` is non-nil if there was an
error in config.EnvFromRC. We depend on it remaining non-nil until the
return statement. cf9222e inadvertently unconditionally overwrote `err`,
causing this function to never report errors from config.EnvFromRC.
Fixed by giving the new error a different name.

You can test the issue by editing this repo's .envrc file and then
running `go run . export bash`.

Fixes #1463

internal/cmd/cmd_export.go
@@ -104,9 +104,9 @@ func exportCommand(currentEnv Env, args []string, config *Config) (err error) {
 		logStatus(config, "export %s", out)
 	}
 
-	diffString, err := currentEnv.Diff(newEnv).ToShell(shell)
-	if err != nil {
-		return fmt.Errorf("ToShell() failed: %w", err)
+	diffString, diffErr := currentEnv.Diff(newEnv).ToShell(shell)
+	if diffErr != nil {
+		return fmt.Errorf("ToShell() failed: %w", diffErr)
 	}
 	logDebug("env diff %s", diffString)
 	fmt.Print(diffString)