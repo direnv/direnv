b85761afc5862fd2e7e6ccab9a854e7cadcb2886
Biggie <code@bigo.lu>
2026-02-24 21:11:33 -0500

    Change spaces to tabs in bash hook

--- internal/cmd/shell_bash.go
+++ internal/cmd/shell_bash.go
@@ -9,19 +9,19 @@ var Bash Shell = bash{}
 
 const bashHook = `
 _direnv_hook() {
-  local previous_exit_status=$?;
-  vars="$("{{.SelfPath}}" export bash)";
-  trap -- '' SIGINT;
-  eval "$vars";
-  trap - SIGINT;
-  return $previous_exit_status;
+	local previous_exit_status=$?;
+	vars="$("{{.SelfPath}}" export bash)";
+	trap -- '' SIGINT;
+	eval "$vars";
+	trap - SIGINT;
+	return $previous_exit_status;
 };
 if [[ ";${PROMPT_COMMAND[*]:-};" != *";_direnv_hook;"* ]]; then
-  if [[ "$(declare -p PROMPT_COMMAND 2>&1)" == "declare -a"* ]]; then
-    PROMPT_COMMAND=(_direnv_hook "${PROMPT_COMMAND[@]}")
-  else
-    PROMPT_COMMAND="_direnv_hook${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
-  fi
+	if [[ "$(declare -p PROMPT_COMMAND 2>&1)" == "declare -a"* ]]; then
+		PROMPT_COMMAND=(_direnv_hook "${PROMPT_COMMAND[@]}")
+	else
+		PROMPT_COMMAND="_direnv_hook${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
+	fi
 fi
 `
 
